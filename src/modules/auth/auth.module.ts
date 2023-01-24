29e38a65032505b71e0532c99b0b6aec72732d82
Petr Koula <koula.petr@gmail.com>
2023-01-24 14:40:33 +0100

    feat: authentication endpoints returns jwt token

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,11 +3,34 @@ import { AuthController } from './auth.controller'
 import { UserModule } from '../user/user.module'
 import { CommonModule } from '../common/common.module'
 import { AuthService } from './auth.service'
+import { ConfigService } from '../common/config/config.service'
+import { JwtModule } from '@nestjs/jwt'
+import { JwtModuleAsyncOptions } from '@nestjs/jwt/dist/interfaces/jwt-module-options.interface'
+
+const jwtModuleOptions: JwtModuleAsyncOptions = {
+  imports: [CommonModule],
+  useFactory: async (configService: ConfigService) => {
+    const expirationSeconds = Number(configService.get('JWT_EXPIRATION_SECONDS'))
+    const secret = configService.get('JWT_SECRET_KEY')
+    return {
+      secret: secret,
+      signOptions: {
+        expiresIn: Number(expirationSeconds),
+      },
+    }
+  },
+  inject: [ConfigService],
+}
 
 @Module({
-  imports: [CommonModule, UserModule],
+  imports: [
+    CommonModule,
+    UserModule,
+    JwtModule.registerAsync(jwtModuleOptions),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule {
+}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,20 +1,41 @@
 import { Injectable } from '@nestjs/common'
 import { HashService } from '../common/security/hash.service'
 import { UserService } from '../user/user.service'
+import { JwtService } from '@nestjs/jwt'
+import { ConfigService } from '../common/config/config.service'
+
+interface JwtToken {
+  expiresIn: number,
+  accessToken: string,
+}
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly hashService: HashService,
-  ) {}
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {
+  }
 
-  async isAuthenticated(email: string, password: string): Promise<boolean> {
+  async authenticate(email: string, password: string): Promise<JwtToken | null> {
     const user = await this.userService.findOneByEmail(email)
     if (!user) {
-      return false
+      return null
     }
 
-    return this.hashService.compare(password, user.password_hash)
+    if (!this.hashService.compare(password, user.password_hash)) {
+      return null
+    }
+
+    return this.createToken(email)
+  }
+
+  private createToken(email: string): JwtToken {
+    const expiresIn = Number(this.configService.get('JWT_EXPIRATION_SECONDS'))
+    const accessToken = this.jwtService.sign({ email })
+
+    return { expiresIn, accessToken }
   }
 }
